1827964dd709588ab58564c120d73fc22a1169c2
Bazyli <bazyli.bednarz@gmail.com>
2021-09-29 01:33:15 +0200

    Added time-aware responsive timeline

--- js/main.js
+++ js/main.js
@@ -2,26 +2,39 @@
  * Burger menu toggling
  */
 
-$('.navigation-burger').on('click', function() {
+$('.navigation-burger').on('click', function () {
     $(this).toggleClass('active');
     if ($(this).hasClass('active')) {
         $(this).html('<i class="fas fa-times"></i>');
         $('.navigation').css('transform', 'translateX(0)');
-    }
-    else {
+    } else {
         $(this).html('<i class="fas fa-bars"></i>')
         $('.navigation').css('transform', 'translateX(100%)');
     }
 });
 
 /**
- * Show heaeder after scrolling past first section
+ * Show header after scrolling past first section
  */
 
-$(window).on('scroll', function() {
+$(window).on('scroll', function () {
     if ($(this).scrollTop() > $('.section-main').height()) {
         $('.header').addClass('active');
     } else {
         $('.header').removeClass('active');
     }
 })
+
+/** 
+ * Handle timeline dates
+ */
+
+const datesArray = ['2021-11-15', '2021-12-1', '2021-12-10', '2021-12-16', '2021-12-20', '2022-01-05', '2022-01-11'];
+const today = new Date();
+for (let i = 0; i < datesArray.length; i++) {
+    if (today > new Date(datesArray[i])) {
+        console.log(new Date(datesArray[i]));
+
+        $('.timeline-list li' + ':nth-of-type(' + (i + 1) + ')').addClass('active');
+    }
+}
